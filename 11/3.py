f8985dcedb9e91971dc28a735f0561b76de2020a
drumgiovanni <giovannitheworld@gmail.com>
2018-01-15 16:28:52 +0900

    share

--- 11/3.py
+++ 11/3.py
@@ -1,15 +1,15 @@
 def deeprev(lst):
-        if not isinstance(lst, list):
-            if lst == []:
-                return []
-            else:
-                return deeprev(lst[1:]) + [lst[0]]
+        if lst == []:
+            return []
+        elif isinstance(lst, list):
+            return
+
         else:
-            for o in lst:
-                if o == []:
-                    return []
-                else:
-                    return deeprev(o[1:]) + [o[0]]
+            return deeprev(lst[1:]) + [lst[0]]
+
+
+
+
 
 print(f"{deeprev([0, 1, [2, 3]])}")
 
